7bce83f17bb49fad0f1a68d4320d6087531599de
drmcarthur <mcarthur.dr@gmail.com>
2014-03-31 15:47:27 -0600

    Added Clear button with javascript & made minor CSS changes.

--- js/calculate.js
+++ js/calculate.js
@@ -1,27 +1,27 @@
 
 // Generate a table with desired material properties
-function addRow(material) {
-    $("#dataTable").append("<tbody>");
+var chosenMaterial = "";	// Store the currently selected material
+var chosenTemp = 0;		// Store the currently entered temperature
+function addRow() {
     var trow = "<tr>";
     	//alert(window[material].props["T"][0]);
 	//var myArray = [Infinity,NaN,10];
 	//alert(1 + myArray[1] );
-    for (var prop in window[material])
+    for (var prop in window[chosenMaterial])
     {
 
 	if (prop == Array) {
-	    trow += "<td data-pname='" + window[material][prop][0] + "'>" + window[material].props[prop][0] + "</td>";
+	    trow += "<td data-pname='" + window[chosenMaterial][prop][0] + "'>" + window[chosenMaterial].props[prop][0] + "</td>";
 	    alert("got here...");
 	}
 	else {
-	    trow += "<td data-pname='" + window[material][prop] + "'>" + window[material][prop] + "</td>";
+	    trow += "<td data-pname='" + window[chosenMaterial][prop] + "'>" + window[chosenMaterial][prop] + "</td>";
 	}
 	
     }
     trow += "</tr>"
-    $("#dataTable").append(trow);
-    $("#dataTable").append("</tbody>");
-    $("#dataTable").css("display", "block");
+    $("#dataTableBody").append(trow);
+    $("#dataTableBody").css("display", "block");
 }
 
 $( document ).ready(function() {
@@ -33,5 +33,17 @@ $(".material").click(function() {
     //$("#matSelector").hide();
     $("#materialText").html('' + this.innerHTML + '<br/>');
     $("#tempRange").html('T = ' + window[this.dataset.material].tMin + ' - ' +
-			 window[this.dataset.material].tMax + ' K'); 
+			 window[this.dataset.material].tMax + ' K');
+    chosenMaterial = window[this.dataset.material].name;
+});
+
+// Clear the table of calculated properties
+$("#clearTable").click(function() {
+    chosenMaterial = "";		// Reset the currently selected material
+    chosenTemp = 0;			// Reset the currently enetered temperature
+    
+    // Remove all displayed properties and deselect a material at the top
+    $("#materialText").html("");	// Hide the selected material display
+    $("#tempRange").html("");
+    $("#dataTableBody").html("");
 });
